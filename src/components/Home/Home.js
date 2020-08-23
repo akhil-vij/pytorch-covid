aa5760f257d733ac812a896281b88c9a5935381d
Akhil <akhilvij@gmail.com>
2020-08-23 09:23:53 +0530

    Home page changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 
 function Home(props) {
   return (
@@ -8,7 +9,31 @@ function Home(props) {
         <span className="app__sidebaritem-label">Let's Go</span>
       </Link> */}
       <div className="app__home-container">
-        <div className="app__home-text-container"></div>
+        <div className="app__home-text-container">
+          <h1 className="app__home-heading"> FIGHT COVID-19</h1>
+          <p className="app__home-text">
+            <div className="app__home-text-headline">
+              Level up your Covid fight skills
+            </div>
+            <ul className="app__home-text-list">
+              <li>Classify Patient chest X-ray as Covid/Pneumonia/Normal</li>
+              <li>
+                Learn how Covid X-ray differs from other chest infections{" "}
+              </li>
+              <li>Study publicly available Covid chest X-ray images</li>
+              <li>
+                Make a note of your observations and generate shareable report
+              </li>
+            </ul>
+          </p>
+          <div className="app__home-button-container">
+            <Button className="app__home-button">
+              <Link to="/classify" className="app__sidebaritem-link">
+                <span className="app__sidebaritem-label">Let's Go</span>
+              </Link>
+            </Button>
+          </div>
+        </div>
         <div className="app__home-image"></div>
       </div>
     </div>
